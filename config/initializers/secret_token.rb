d9d660c6473e0d0659f74eeb617a527036617f11
Swing Zhou <swing.zhou@hotmail.com>
2012-02-18 23:49:59 +0800

    bootcamp on rails 3.x: depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '29228156024a0365071d50e823c90355257c3cb232e1ff52358732d9d0e2d774908a5a6000b1ce420afb30a4ad09d7863843539c6442ed76fcf53e0119d6c186'
